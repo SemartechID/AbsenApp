76e613f44a369c2b96c37c0b9b6b5af100e98ec4
Raqael Fisabillah <raqaelfisabillah@gmail.com>
2020-05-12 03:27:10 +0700

    edit user type

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('isSuper')->default(false);
-            $table->boolean('isHead')->default(false);
+            $table->boolean('isAdmin')->default(false);
             $table->boolean('isMember')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
